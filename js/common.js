8eb7d175f510894fd950e079e663c20564cb3947
Fotoz <fotozxx@gmail.com>
2017-09-16 13:30:13 +0300

    first commit

---
+++ js/common.js
@@ -0,0 +1,12 @@
+$(function() {
+
+//smooth-scroling
+    var $page = $('html, body');
+        $('a[href*="#"]').click(function() {
+            $page.animate({
+                scrollTop: $($.attr(this, 'href')).offset().top
+            }, 800);
+            return false;
+    });
+
+});
