469150e069a6dac8e4ca1ad00b2b7442ace9cfb1
Fotoz <fotozxx@gmail.com>
2017-09-21 12:06:58 +0300

    header with mmenu

--- js/common.js
+++ js/common.js
@@ -1,12 +1,32 @@
 $(function() {
 
+// my-menu
+	$('#my-menu').mmenu({
+		extensions: [ 'theme-dark', 'fx-listitems-slide', 'pagedim-black' ],
+		navbar: {
+			title: 'investor<strong>agency</strong>'
+		},
+		offCanvas: {
+			position: 'right'
+		}
+	});
+
+// hamburger
+	var api = $('#my-menu').data('mmenu');
+	api.bind('open:finish', function() {
+		$('.hamburger').addClass('is-active');
+	});
+	api.bind('close:finish', function() {
+		$('.hamburger').removeClass('is-active');
+	});
+
 //smooth-scroling
-    var $page = $('html, body');
-        $('a[href*="#"]').click(function() {
-            $page.animate({
-                scrollTop: $($.attr(this, 'href')).offset().top
-            }, 800);
-            return false;
-    });
+    // var $page = $('html, body');
+    //     $('a[href*="#"]').click(function() {
+    //         $page.animate({
+    //             scrollTop: $($.attr(this, 'href')).offset().top
+    //         }, 800);
+    //         return false;
+    // });
 
 });
