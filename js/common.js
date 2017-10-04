6d3619c493f1a7a13ea5d392508a92932d14b0c3
Fotoz <fotozxx@gmail.com>
2017-10-04 17:59:18 +0300

    our-services

--- js/common.js
+++ js/common.js
@@ -20,7 +20,20 @@ $(function() {
 		$('.hamburger').removeClass('is-active');
 	});
 
-//smooth-scroling
+// equal-height
+	function setEqualHeight(columns) {
+	var tallestcolumn = 0;
+	columns.each(function() {
+	currentHeight = $(this).height();
+	if(currentHeight > tallestcolumn) {
+		tallestcolumn  = currentHeight;
+	}
+	});
+	columns.height(tallestcolumn);
+	}
+	setEqualHeight($(".services-item"));
+
+// smooth-scroling
     // var $page = $('html, body');
     //     $('a[href*="#"]').click(function() {
     //         $page.animate({
